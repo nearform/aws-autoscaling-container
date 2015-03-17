c446a254231cb1b1907991ea56680663849b4ea6
Matteo Collina <matteo.collina@gmail.com>
2015-03-17 16:31:58 +0000

    Added blank container

---
+++ container.js
@@ -0,0 +1,144 @@
+/*
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+'use strict';
+
+var bunyan = require('bunyan');
+
+module.exports = function(config, logger) {
+
+  logger = logger || logger.createLogger({name: 'blank-container'});
+
+
+
+  /**
+   * build the container 
+   * cdef - contianer definition block
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  /*
+   * commented out as it the blank-container does not need any
+   * specific building.
+  var build = function build(mode, system, cdef, out, cb) {
+    logger.info('building');
+    out.stdout('building');
+    cb(null, {});
+  };
+ */
+
+
+
+  /**
+   * deploy the continaer
+   * target - target to deploy to
+   * system - the target system defintinion
+   * cdef - the contianer definition
+   * container - the container as defined in the system topology
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  var deploy = function deploy(mode, target, system, containerDef, container, out, cb) {
+    cb();
+  };
+
+
+
+  /**
+   * undeploy the container from the target
+   * target - target to deploy to
+   * system - the target system defintinion
+   * cdef - the contianer definition
+   * container - the container as defined in the system topology
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  var undeploy = function undeploy(mode, target, system, containerDef, container, out, cb) {
+    cb();
+  };
+
+
+
+  /**
+   * start the container on the target
+   * target - target to deploy to
+   * system - the target system defintinion
+   * cdef - the contianer definition
+   * container - the container as defined in the system topology
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  var start = function start(mode, target, system, containerDef, container, out, cb) {
+    cb();
+  };
+
+
+
+  /**
+   * stop the container on the target
+   * target - target to deploy to
+   * system - the target system defintinion
+   * cdef - the contianer definition
+   * container - the container as defined in the system topology
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  var stop = function stop(mode, target, system, containerDef, container, out, cb) {
+    cb();
+  };
+
+
+
+  /**
+   * link the container to the target
+   * target - target to deploy to
+   * system - the target system defintinion
+   * cdef - the contianer definition
+   * container - the container as defined in the system topology
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  var link = function link(mode, target, system, containerDef, container, out, cb) {
+    cb();
+  };
+
+
+
+  /**
+   * unlink the container from the target
+   * target - target to deploy to
+   * system - the target system defintinion
+   * cdef - the contianer definition
+   * container - the container as defined in the system topology
+   * out - ouput stream 
+   * cb - complete callback
+   */
+  var unlink = function unlink(mode, target, system, containerDef, container, out, cb) {
+    cb();
+  };
+
+
+
+  return {
+    deploy: deploy,
+    start: start,
+    stop: stop,
+    link: link,
+    unlink: unlink,
+    undeploy: undeploy,
+    add: deploy,
+    remove: undeploy
+  };
+};
+
